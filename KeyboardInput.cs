9ff2dd9eade5198cffcfe8496f6f92639a118cb7
Edward Seley <edwardseley@gmail.com>
2017-07-05 19:59:36 -0500

    Added mouse and keyboard control for player

---
+++ KeyboardInput.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardInput : MonoBehaviour {
+    public float speed = 4.0f;
+
+	void Update () {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+        transform.Translate(deltaX, 0, deltaZ);
+	}
+}
