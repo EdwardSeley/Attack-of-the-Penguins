b2a05cae975730ed0488b615830aab8dfbbbbe13
Edward Seley <edwardseley@gmail.com>
2017-07-26 20:43:43 -0500

    Added igloo model and reprogrammed shooter

---
+++ PenguinController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PenguinController : MonoBehaviour {
+
+    public float speed =  3.0f;
+    public float obstacleRange = 9.0f;
+
+    public Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.speed = speed * 0.75f;
+    }
+
+    public void ReactToHit()
+    {
+
+    }
+	void Update ()
+    {
+        transform.Translate(0, 0, speed * Time.deltaTime);
+        Vector3 currentPosition = transform.position;
+        currentPosition.y *= 0.5f;
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        if (Physics.SphereCast(ray, 1f, out hit))
+        {
+            if (hit.distance < obstacleRange)
+            {
+                float angle = Random.Range(-110, 110);
+                transform.Rotate(0, angle, 0);
+            }
+        }
+	}
+}
---
+++ SnowballShooter.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowballShooter : MonoBehaviour
+{
+    private Camera _camera;
+
+    void Start()
+    {
+        _camera = GetComponent<Camera>();
+    }
+
+    private void OnGUI()
+    {
+        int size = 12;
+        float posX = _camera.pixelWidth / 2 - size / 4;
+        float posY = _camera.pixelHeight / 2 - size / 2;
+        GUI.Label(new Rect(posX, posY, size, size), "*");
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 point = new Vector3(_camera.pixelWidth / 2, _camera.pixelHeight / 2, 0);
+            Ray ray = _camera.ScreenPointToRay(point);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                GameObject hitObject = hit.transform.gameObject;
+                PenguinController penguin = hitObject.GetComponent<PenguinController>();
+                if (penguin != null)
+                {
+                    Debug.Log("Penguin hit!");
+                }
+                StartCoroutine(SnowballIndicator(hit.point));
+            }
+        }
+    }
+
+    private IEnumerator SnowballIndicator(Vector3 pos)
+    {
+        GameObject snowball = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        snowball.transform.position = pos;
+
+        yield return new WaitForSeconds(1);
+
+        Destroy(snowball);
+    }
+}
